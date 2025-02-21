b924a0221720e98002781cd927d9eceefe996126
Nathalie Reinke <nathalie.reinke@volkswagen.de>
2025-02-21 08:09:22 +0100

    Route für Donation hinzugefügt

--- src/controller/donation-controller.ts
+++ src/controller/donation-controller.ts
@@ -2,15 +2,26 @@ import { getPool } from "../db/db.js";
 import type { Donation } from "../types.ts";
 
 export class DonationController {
+  static async getAllDonations(){
+    try{
+      const res = await getPool().query(`SELECT * FROM "Donations"`);
+      return res.rows;
+    }
+    catch(error){
+      console.error("Fehler beim Abrufen aller Spenden",error);
+      return error;
+    }
+  }
   static async insertDonation(donation: Donation) {
     try {
       const res = await getPool().query(
-        `INSERT INTO "Donation" ("person_name","amount","date","pdf_url") VALUES($1,$2,$3)`,
+        `INSERT INTO "Donations" ("person_name","amount","date","pdf_url") VALUES($1,$2,$3,$4) RETURNING *`,
         [donation.person_name, donation.amount, donation.date, donation.pdf_url]
       );
       return res.rows[0];
     } catch (error) {
       console.error("Fehler beim Speichern der Spende.", error);
+      return error;
     }
   }
 }
--- src/routes/donation-routes.ts
+++ src/routes/donation-routes.ts
@@ -3,8 +3,20 @@ import { DonationController } from "../controller/donation-controller.js";
 
 export const donationRouter = new Hono();
 
+donationRouter.get("/", async (c) => {
+  try{
+    const donations = DonationController.getAllDonations();
+    return c.json({donations},200);
+  }
+  catch(error){
+    return c.json({error:error},500);
+  }
+})
 donationRouter.post("/insertDonation", async (c) => {
     const { person_name, amount, date, pdf_url } = await c.req.json();
+    if( amount <= 0){
+      return c.json({message:'Die Spende muss größer als 0€ sein.'},405)
+    }
     const donationData = {
       person_name,
       amount,
@@ -13,7 +25,10 @@ donationRouter.post("/insertDonation", async (c) => {
     };
   
     try {
-      await DonationController.insertDonation(donationData);
+      const res = await DonationController.insertDonation(donationData);
+      if(!res){
+        return c.json({message: "Irgendwas ist schief gelaufen.."},404);
+      }
       return c.json({ message: "Spende wurde erfolgreich hinzugefügt" }, 201);
     } catch (error) {
       return c.json({ error: "Fehler beim Speichern der Spende" }, 500);
